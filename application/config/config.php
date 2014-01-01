b5b01446dcaca450e105b9595094a18bf6b64398
sureshprasanna70 <sureshprasanna70@gmail.com>
2014-01-02 00:20:56 +0530

    Added .htaceess

--- application/config/config.php
+++ application/config/config.php
@@ -14,7 +14,7 @@
 | path to your installation.
 |
 */
-$config['base_url']	= 'http://localhost/ros/';
+$config['base_url']	= 'http://localhost/ros';
 // $config['base_url']	= 'http://kurukshetra.org.in/';
 /*
 |--------------------------------------------------------------------------
