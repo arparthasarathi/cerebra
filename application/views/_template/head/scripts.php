20f5aa69af2daf5079092d7ac4b8940526dadb48
arparthasarathi <arparthasarathi@gmail.com>
2014-01-18 17:44:49 +0530

    base url of script changed

--- application/views/_template/head/scripts.php
+++ application/views/_template/head/scripts.php
@@ -1,5 +1,5 @@
 		<script type="text/javascript">
-			var base_url = "<? echo base_url();?>";			
+			var base_url = "http://kurukshetra.org.in/";			
 			 
 		</script>
 		<script type="text/javascript" src="<? echo base_url();?>/assets/scripts/jquery.min.js"></script>
