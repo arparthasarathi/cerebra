b28d5ebae83f36ac64a7cb1759c4aa76bc58103d
arparthasarathi <arparthasarathi@gmail.com>
2015-08-11 14:44:56 +0530

    Source code of final successful build of Cerebra

---
+++ application/views/_template/basic/forum.php
@@ -0,0 +1,7 @@
+ <title>CEREBRA|FORUM</title><div class="white-head">Official Forum
+
+   
+<div class="comment">
+<div class="fb-comments" data-href="http://kurukshetra.org.in/cerebra/forum" data-width="500" data-numposts="5" data-colorscheme="light"></div>
+  </div>
+  </div>
